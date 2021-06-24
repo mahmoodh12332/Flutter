1022bb2f5b83c6612c61114af67de95b96fec96e
Mahmood Hassan <32261587+mahmoodh12332@users.noreply.github.com>
2021-06-24 18:27:23 +0500

    flutter-1

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,5 @@
 import "package:flutter/material.dart";
 
-import 'app_screen/first_screen.dart';
-
-void main() => runApp(MyFlutterappClass());
-
-class MyFlutterappClass extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: "Flutter App",
-      theme: ThemeData(
-        appBarTheme: AppBarTheme(backgroundColor: Colors.deepPurple),
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("MY First App"),
-        ),
-        body: FirstScreen(),
-      ),
-    );
-  }
+void main() {
+  runApp();
 }
