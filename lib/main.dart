2323f096360fe0c93b8d4f37171979574d7414b4
Mahmood Hassan <32261587+mahmoodh12332@users.noreply.github.com>
2021-06-24 17:57:02 +0500

    flutter-1

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,29 @@ import "package:flutter/material.dart";
 
 void main() {
   runApp(
-    Center(
-      child: Text(
-        "Flutter Hello",
-        textDirection: TextDirection.ltr,
-        textAlign: TextAlign.center,
+    MaterialApp(
+      title: "Flutter App",
+      theme: ThemeData(
+        appBarTheme: AppBarTheme(backgroundColor: Colors.deepPurple),
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("MY First App"),
+        ),
+        body: Material(
+          color: Colors.purple,
+          child: Center(
+            child: Text(
+              "Flutter Hello",
+              textDirection: TextDirection.ltr,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 30.0,
+              ),
+            ),
+          ),
+        ),
       ),
     ),
   );
